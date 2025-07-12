7c676eb5926e3f7c681714017c81e614645d44d4
oghodo omoregbe <omoregbeoghodo54@gmail.com>
2025-07-13 00:55:46 +0100

    My new changes

--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -57,3 +57,37 @@ internal class PriorityItem
         return $"{Value} (Pri:{Priority})";
     }
 }
+public class PriorityQueue
+{
+    private List<(string Value, int Priority)> _queue = new List<(string, int)>();
+
+    public void Enqueue(string value, int priority)
+    {
+        _queue.Add((value, priority)); // Add to the end (FIFO)
+    }
+
+    public string Dequeue()
+    {
+        if (_queue.Count == 0)
+        {
+            throw new InvalidOperationException("Queue is empty");
+        }
+
+        int maxPriority = int.MinValue;
+        int indexOfMax = -1;
+
+        // Find first occurrence of highest priority
+        for (int i = 0; i < _queue.Count; i++)
+        {
+            if (_queue[i].Priority > maxPriority)
+            {
+                maxPriority = _queue[i].Priority;
+                indexOfMax = i;
+            }
+        }
+
+        string value = _queue[indexOfMax].Value;
+        _queue.RemoveAt(indexOfMax); // Remove first highest-priority item
+        return value;
+    }
+}
--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -55,3 +55,37 @@ public class TakingTurnsQueue
         return _people.ToString();
     }
 }
+public class TakingTurnsQueue
+{
+    private Queue<Person> _queue = new Queue<Person>();
+
+    public void AddPerson(string name, int turns)
+    {
+        _queue.Enqueue(new Person(name, turns));
+    }
+
+    public string GetNextPerson()
+    {
+        if (_queue.Count == 0)
+        {
+            throw new InvalidOperationException("Queue is empty");
+        }
+
+        Person person = _queue.Dequeue();
+
+        if (person.Turns <= 0)
+        {
+            _queue.Enqueue(person);
+        }
+        else
+        {
+            person.Turns--;
+            if (person.Turns > 0)
+            {
+                _queue.Enqueue(person);
+            }
+        }
+
+        return person.Name;
+    }
+}
